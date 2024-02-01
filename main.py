c57e0784d45502de468ba74f99df1dfe2ab4e0f3
LukeNoles <89222532+LukeNoles@users.noreply.github.com>
2024-02-01 12:36:24 -0600

    update

--- main.py
+++ main.py
@@ -1,13 +1,13 @@
 def check_character(word, index):
    char =  word[index]
    if str(char).isalpha():
-       return("letter")
+       return "letter"
    if str(char).isdigit():
-       return("digit")
+       return "digit"
    if str(char).isspace():
-       return("whitespace")
+       return "whitespace"
    else:
-       return("unknown")
+       return "unknown"
 
 if __name__ == '__main__': 
     print(check_character('happy birthday', 2))
